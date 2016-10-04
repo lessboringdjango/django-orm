c1b16667ac070e8b644fc09610bf7eb9cbecd634
David Colgan <dvcolgan@gmail.com>
2016-10-04 12:17:11 -0400

    remove answers

--- player/models.py
+++ player/models.py
@@ -1,12 +1,3 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
-
-class Pizza(models.Model):
-    name = models.CharField(max_length=255)
-#
-#
-#class Topping(models.Model):
-#    pizza = models.ForeignKey(Pizza)
-#    name = models.CharField(max_length=255)
